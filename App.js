01f435a98a71a70ea95c5fbd70020cb850067ac2
ethan.mcneal <ethan.mcneal@gmail.com>
2021-06-07 14:14:29 -0600

    enableScreens from native-screens

--- App.js
+++ App.js
@@ -1,40 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import * as Font from 'expo-font'
-import AppLoading from 'expo-app-loading';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+import { enableScreens } from "react-native-screens";
+import MealsNavigator from "./navigation/MealsNavigator";
 
-import MealsNavigator from './navigation/MealsNavigator'
-
+enableScreens();
 const fetchFonts = () => {
-    return Font.loadAsync({
-      'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-      'bold-sans': require('./assets/fonts/OpenSans-Bold.ttf')
-    })
-}
+	return Font.loadAsync({
+		"open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+		"bold-sans": require("./assets/fonts/OpenSans-Bold.ttf"),
+	});
+};
 export default function App() {
+	const [fontLoaded, setFontLoaded] = useState(false);
 
-  const [fontLoaded, setFontLoaded] = useState(false)
+	if (!fontLoaded) {
+		return (
+			<AppLoading
+				startAsync={fetchFonts}
+				onFinish={() => setFontLoaded(true)}
+				onError={(err) => console.log(err)}
+			/>
+		);
+	}
 
-  if(!fontLoaded){
-    return  (
-    <AppLoading
-    startAsync={fetchFonts}
-    onFinish={() => setFontLoaded(true)}
-    onError={(err) => console.log(err)} />
-    )
-  }
-  
-  return (
-    <MealsNavigator />
-  );
+	return <MealsNavigator />;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
