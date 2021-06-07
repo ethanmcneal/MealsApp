1eae017d065273e5a1bc08cadb0e4b7739bbaa25
ethan.mcneal <ethan.mcneal@gmail.com>
2021-06-07 13:18:11 -0600

    prettier

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import { Button, StyleSheet, Text, View } from 'react-native'
-
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 const CategoriesScreen = (props) => {
-    return(
-        <View style={styles.screen}>
-            <Text>
-                Cat screen
-            </Text>
-            <Button title='go to meals!' onPress={() => props.navigation.navigate({routeName: 'Meals'})}/>
-        </View>
-    )
-}
+  return (
+    <View style={styles.screen}>
+      <Text>Cat screen</Text>
+      <Button
+        title="go to meals!"
+        onPress={() => props.navigation.navigate({ routeName: "Meals" })}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-})
-export default CategoriesScreen
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+export default CategoriesScreen;
--- screens/MealsScreen.js
+++ screens/MealsScreen.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
-
-const MealsScreen = props => {
-    return(
-        <View style={styles.screen}>
-            <Text>
-                meal screen
-            </Text>
-            <Button title='Go to Meal Details' onPress={() => props.navigation.navigate({routeName: 'MealDetails'})}/>
-        </View>
-    )
-}
+const MealsScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text>meal screen</Text>
+      <Button
+        title="Go to Meal Details"
+        onPress={() => props.navigation.navigate({ routeName: "MealDetails" })}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-})
-export default MealsScreen
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+export default MealsScreen;
