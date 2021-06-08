733855cf0dfb03a50fd3ce6c1e038b3ca84e123a
ethan.mcneal <ethan.mcneal@gmail.com>
2021-06-08 17:14:36 -0600

    drawaer button on favorites screen

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import CustomHeaderButton from "../components/HeaderButton";
 import MealList from "../components/MealList";
 import {MEALS} from '../data/dummy-data'
 
@@ -9,7 +11,20 @@ const FavoritesScreen = (props) => {
 	);
 };
 
-FavoritesScreen.navigationOptions = {
-    headerTitle: 'Favorite Meals'
+FavoritesScreen.navigationOptions = (navData) => {
+    return {
+		headerTitle: "Favorites Meals",
+		headerLeft: () => {
+			return (
+				<HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+					<Item
+						title="Menu"
+						iconName="ios-menu"
+						onPress={() => navData.navigation.toggleDrawer()}
+					/>
+				</HeaderButtons>
+			);
+		},
+	};
 }
 export default FavoritesScreen;
