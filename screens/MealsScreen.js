7dc1707def23c2a9243a3e9c68db3a100cdfd04e
ethan.mcneal <ethan.mcneal@gmail.com>
2021-06-10 16:03:06 -0600

    fall back text if filters cause no meals to render in a category

--- screens/MealsScreen.js
+++ screens/MealsScreen.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { StyleSheet, View } from "react-native";
 import { useSelector } from 'react-redux'
+import DefaultText from "../components/DefaultText";
 import MealList from "../components/MealList";
 import { CATEGORIES} from "../data/dummy-data";
 
 const MealsScreen = (props) => {
 	const catId = props.navigation.getParam("categoryId");
-
+    const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
     const availableMeals = useSelector(state => state.meals.filteredMeals);
 	const meals = availableMeals.filter((meal) => meal.categoryIds.indexOf(catId) >= 0);
+
+    if (meals.length === 0){
+        return (
+            <View style={styles.screen}>
+                <DefaultText>No meals to view in {selectedCategory.title}, check your filters!</DefaultText>
+            </View>
+        )
+    }
 	return (
 		<MealList meals={meals} navigation={props.navigation}/>
 	);
@@ -21,4 +31,12 @@ MealsScreen.navigationOptions = (navigationData) => {
 		headerTitle: selectedCategory.title,
 	};
 };
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
 export default MealsScreen;
