c59fc352578961863447033ba6f7736e7be9eb1f
ethan.mcneal <ethan.mcneal@gmail.com>
2021-06-10 14:19:05 -0600

    redux instead of import on mealsscreen

--- screens/MealsScreen.js
+++ screens/MealsScreen.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { useSelector } from 'react-redux'
 import MealList from "../components/MealList";
-import { CATEGORIES, MEALS } from "../data/dummy-data";
+import { CATEGORIES} from "../data/dummy-data";
 
 const MealsScreen = (props) => {
 	const catId = props.navigation.getParam("categoryId");
-	const meals = MEALS.filter((meal) => meal.categoryIds.indexOf(catId) >= 0);
+
+    const availableMeals = useSelector(state => state.meals.filteredMeals);
+	const meals = availableMeals.filter((meal) => meal.categoryIds.indexOf(catId) >= 0);
 	return (
 		<MealList meals={meals} navigation={props.navigation}/>
 	);
