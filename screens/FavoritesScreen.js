4c8c8026d0739688d9a336f57eba5a6a398b1ca9
ethan.mcneal <ethan.mcneal@gmail.com>
2021-06-10 15:10:23 -0600

    default text if no fav meals

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -3,14 +3,32 @@ import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import CustomHeaderButton from "../components/HeaderButton";
 import MealList from "../components/MealList";
 import {useSelector} from 'react-redux'
+import { StyleSheet, Text, View } from "react-native";
+import DefaultText from "../components/DefaultText";
 
 const FavoritesScreen = (props) => {
 
     const favMeals = useSelector(state => state.meals.favoriteMeals)
+
+    if (favMeals.length === 0 || !favMeals){
+        return <View style={styles.screen}>
+            <DefaultText>
+                No Favorite Meals
+            </DefaultText>
+        </View>
+    }
 	return (
 		<MealList meals={favMeals} navigation={props.navigation}/>
 	);
 };
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
 
 FavoritesScreen.navigationOptions = (navData) => {
     return {
